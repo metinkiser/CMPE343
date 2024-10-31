a6bc3263122a28a7eaae8f350baf7cf338b2a124
rabiaaygun <151653389+rabiaaygun@users.noreply.github.com>
2024-11-01 02:10:35 +0300

    Update Main.java

--- src/oop/Main.java
+++ src/oop/Main.java
@@ -11,7 +11,7 @@ public class Main {
 	 */
     public static void main(String[] args) {
     	
-        // ASCII sanatı çıktısı
+        // ASCII 
         System.out.println("█     █  █▀▀  █     ▄▀▀▀   ▄▀▀▀▄   █▀▄ ▄▀█  █▀▀  █");
         System.out.println("█  ▄  █  █■■  █    ■      ■     ■  █  ▀  █  █■■  █");
         System.out.println("█▄▀ ▀▄█  █▄▄  █▄▄   ▀▄▄▄   ▀▄▄▄▀   █     █  █▄▄  ▄ ");
