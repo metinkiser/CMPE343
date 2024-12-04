eaac5a5329fe303df0e82f3d456a08136d68046c
rabiaaygun <151653389+rabiaaygun@users.noreply.github.com>
2024-12-04 19:53:57 +0300

    Update DatabaseUtil.java

--- src/project2/DatabaseUtil.java
+++ src/project2/DatabaseUtil.java
@@ -4,13 +4,34 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Utility class for managing database connections.
+ */
 public class DatabaseUtil {
+	 /**
+     * The URL of the database to connect to, including timezone settings.
+     */
+	
     private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/firm_management?useTimezone=true&serverTimezone=UTC";
+    /**
+     * The username required for database access.
+     */
+    
     private static final String DATABASE_USER = "root"; // MySQL username
-    private static final String DATABASE_PASSWORD = "123qwe"; // MySQL password
 
+    /**
+     * The password required for database access.
+     */
+    
+    private static final String DATABASE_PASSWORD = "sifre123"; // MySQL password
+    /**
+     * Establishes a connection to the database using the provided credentials.
+     *
+     * @return a {@link Connection} object that allows interaction with the database.
+     * @throws SQLException if the connection cannot be established or if a database access error occurs.
+     */
+    
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
     }
 }
-
