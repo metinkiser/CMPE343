0c710d0e6fdfe6958951fa8c86b25c922582732f
emirgndgd <97131591+emirgndgd@users.noreply.github.com>
2025-01-11 17:55:41 +0300

    Update LoginController.java

--- src/CinemaCenter/LoginController.java
+++ src/CinemaCenter/LoginController.java
@@ -1,13 +1,12 @@
 package CinemaCenter;
 
-import javafx.fxml.FXML;
+import javafx.fxml.FXML;	
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class LoginController {
@@ -48,6 +47,10 @@ public class LoginController {
                 case "Cashier":
                     loadCashierOperationsScreen();
                     break;
+                case "Manager":
+                    loadManagerOperationsScreen(); // YENİ
+                    break;
+                    
                 default:
                     errorLabel.setText("Bu role henüz destek sağlanmıyor.");
                     break;
@@ -73,4 +76,32 @@ public class LoginController {
             errorLabel.setText("Cashier Operations ekranı yüklenirken hata oluştu.");
         }
     }
+    
+    private void loadManagerOperationsScreen() {
+        try {
+            // Manager'ın userID'sini al
+            int managerId = UserDAO.getUserId(usernameField.getText(), passwordField.getText());
+            if (managerId == -1) {
+                errorLabel.setText("Kullanıcı bilgileri alınamadı.");
+                return;
+            }
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/CinemaCenter/manager_operations.fxml"));
+            Scene scene = new Scene(loader.load());
+            
+            // ManagerController'a userID'yi ilet
+            ManagerController managerController = loader.getController();
+            managerController.setManagerUserId(managerId);
+            
+            Stage stage = (Stage) usernameField.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Manager Operations");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            errorLabel.setText("Manager Operations ekranı yüklenirken hata oluştu.");
+        }
+    }
+    
+    
 }
