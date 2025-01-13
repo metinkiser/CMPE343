2dfc28e07732f28691911b78c186042d1ca0d67b
rabiaaygun <151653389+rabiaaygun@users.noreply.github.com>
2025-01-13 07:49:32 +0300

    Update Product.java

--- src/CinemaCenter/Product.java
+++ src/CinemaCenter/Product.java
@@ -1,5 +1,8 @@
-package CinemaCenter;
+package cinemaCenter;
 
+/**
+ * Represents a Product from the Products table.
+ */
 public class Product {
     private int productID;
     private String name;
@@ -7,6 +10,14 @@ public class Product {
     private int stockQuantity;
     private String category;
 
+    /**
+     * Constructs a Product object.
+     * @param productID The product ID
+     * @param name The product name
+     * @param price The product price
+     * @param stockQuantity The current stock
+     * @param category The product category (Beverage, Snack, Toy, etc.)
+     */
     public Product(int productID, String name, double price, int stockQuantity, String category) {
         this.productID = productID;
         this.name = name;
