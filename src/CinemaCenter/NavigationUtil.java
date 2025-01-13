f801dc495898051933e534655b7b0093cbfba5dc
emirgndgd <97131591+emirgndgd@users.noreply.github.com>
2025-01-13 18:08:41 +0300

    Update NavigationUtil.java

--- src/CinemaCenter/NavigationUtil.java
+++ src/CinemaCenter/NavigationUtil.java
@@ -5,11 +5,32 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Utility class for managing scene loading and navigation in JavaFX application.
+ */
 public class NavigationUtil {
+    
+    /**
+     * Loads a new scene from the specified FXML file.
+     *
+     * @param fxmlPath Path to the FXML file to be loaded
+     * @param title Window title for the scene
+     * @param <T> Type of the controller class
+     * @return Controller instance of the loaded scene
+     */
     public static <T> T loadScene(String fxmlPath, String title) {
         return loadScene(fxmlPath, title, null);
     }
 
+    /**
+     * Loads a new scene from the specified FXML file and initializes the controller with data.
+     *
+     * @param fxmlPath Path to the FXML file to be loaded
+     * @param title Window title for the scene
+     * @param data Data to be passed to the controller
+     * @param <T> Type of the controller class
+     * @return Controller instance of the loaded scene
+     */
     public static <T> T loadScene(String fxmlPath, String title, Object data) {
         try {
             FXMLLoader loader = new FXMLLoader(NavigationUtil.class.getResource(fxmlPath));
@@ -17,7 +38,6 @@ public class NavigationUtil {
             
             T controller = loader.getController();
             
-            // Controller'ın data setter metodunu çağır
             if (data != null && controller instanceof DataInitializable) {
                 ((DataInitializable) controller).initData(data);
             }
