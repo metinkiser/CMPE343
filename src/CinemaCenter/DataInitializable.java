db7c2128a943fa665b7a213ca43c20a666f52e85
emirgndgd <97131591+emirgndgd@users.noreply.github.com>
2025-01-13 18:06:59 +0300

    Update DataInitializable.java

--- src/CinemaCenter/DataInitializable.java
+++ src/CinemaCenter/DataInitializable.java
@@ -1,5 +1,14 @@
 package CinemaCenter;
 
+/**
+ * An interface that defines a contract for objects that can be initialized with data.
+ * Implementing classes should provide logic to initialize their state using the provided data.
+ */
 public interface DataInitializable {
+    /**
+     * Initializes the implementing object with the specified data.
+     *
+     * @param data The data object used to initialize this instance
+     */
     void initData(Object data);
 } 
