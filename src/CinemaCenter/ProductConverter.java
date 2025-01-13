593805240511ef8aded50cfb2784a4199ec72e3d
rabiaaygun <151653389+rabiaaygun@users.noreply.github.com>
2025-01-13 07:50:26 +0300

    Update ProductConverter.java

--- src/CinemaCenter/ProductConverter.java
+++ src/CinemaCenter/ProductConverter.java
@@ -3,10 +3,17 @@ package cinemaCenter;
 import javafx.scene.control.ComboBox;
 import javafx.util.StringConverter;
 
+/**
+ * A converter for Product objects so the ComboBox displays only the product name.
+ */
 public class ProductConverter extends StringConverter<Product> {
 
     private ComboBox<Product> comboBox;
 
+    /**
+     * Constructor that receives the ComboBox reference.
+     * @param comboBox The ComboBox<Product>
+     */
     public ProductConverter(ComboBox<Product> comboBox) {
         this.comboBox = comboBox;
     }
@@ -14,14 +21,11 @@ public class ProductConverter extends StringConverter<Product> {
     @Override
     public String toString(Product object) {
         if (object == null) return "";
-        return object.getName(); 
-        // İsterseniz: return object.getName() + " - " + object.getPrice() + "₺";
+        return object.getName();
     }
 
     @Override
     public Product fromString(String string) {
-        // Normalde pek kullanılmaz, 
-        // comboBox selection üzerinden setValue yapılıyor
         return comboBox.getItems().stream()
                 .filter(p -> p.getName().equals(string))
                 .findFirst()
